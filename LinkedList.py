84c06697362f793e0a3ce10522299f57158dd13a
ashton01L <ashtonamazing@gmail.com>
2024-11-14 01:34:40 -0600

    8th attempt

--- LinkedList.py
+++ LinkedList.py
@@ -22,30 +22,35 @@ class LinkedList:
         return self._head
 
     def add(self, data):
-        """Adds a node with given data at the front of the list."""
-        new_node = Node(data)
-        new_node.next = self._head
-        self._head = new_node
+        """Recursively adds a node with given data at the front of the list."""
+
+        def _add(node, data):
+            """Helper recursive function to add a node at the front."""
+            if node is None:
+                return Node(data)
+            new_node = Node(data)
+            new_node.next = node
+            return new_node
+
+        self._head = _add(self._head, data)
 
     def remove(self):
-        """Removes the last node from the list using recursion."""
+        """Recursively removes the last node from the list."""
 
-        def _remove_last(node):
-            if node.next.next is None:
-                node.next = None
-            else:
-                _remove_last(node.next)
+        def _remove(node):
+            """Helper recursive function to remove the last node."""
+            if node is None or node.next is None:
+                return None
+            node.next = _remove(node.next)
+            return node
 
-        if self._head is not None:
-            if self._head.next is None:
-                self._head = None
-            else:
-                _remove_last(self._head)
+        self._head = _remove(self._head)
 
     def contains(self, data):
-        """Checks if the list contains a node with the given data."""
+        """Recursively checks if list contains a node with the given data."""
 
         def _contains(node):
+            """Helper recursive function to check if data exists."""
             if node is None:
                 return False
             if node.data == data:
@@ -55,9 +60,10 @@ class LinkedList:
         return _contains(self._head)
 
     def insert(self, index, data):
-        """Inserts a node with given data at the specified index."""
+        """Recursively inserts a node with given data at the specified index."""
 
         def _insert(node, index):
+            """Helper recursive function to insert at given index."""
             if index == 0:
                 new_node = Node(data)
                 new_node.next = node
@@ -72,21 +78,23 @@ class LinkedList:
         self._head = _insert(self._head, index)
 
     def reverse(self):
-        """Reverses the linked list in place by rearranging node order."""
+        """Recursively reverses the linked list."""
 
         def _reverse(node, prev=None):
+            """Helper recursive function to reverse the list."""
             if node is None:
                 return prev
             next_node = node.next
             node.next = prev
             return _reverse(next_node, node)
 
-        self._head = _reverse(self._head)
+        self._head = _reverse(self._head, prev=None)
 
     def to_plain_list(self):
-        """Converts the linked list to a regular Python list using recursion."""
+        """Recursively converts the linked list to a regular Python list."""
 
         def _to_plain_list(node):
+            """Helper recursive function to convert list to Python list."""
             if node is None:
                 return []
             return [node.data] + _to_plain_list(node.next)
@@ -97,6 +105,7 @@ class LinkedList:
         """Displays contents of the list."""
 
         def _display(node):
+            """Helper recursive function to display the list."""
             if node is None:
                 return
             print(node.data, end=" -> ")
@@ -106,7 +115,7 @@ class LinkedList:
         print("None")
 
 
-# # Example usage
+# # Example usage and test cases
 # ll = LinkedList()
 # ll.add(3)
 # ll.add(2)
