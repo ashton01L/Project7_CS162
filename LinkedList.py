5e1b8b90207c057d92fadab13330eb5ac80cc434
ashton01L <ashtonamazing@gmail.com>
2024-11-14 01:17:23 -0600

    fifth iteration to test vs. GradeScope

--- LinkedList.py
+++ LinkedList.py
@@ -85,14 +85,13 @@ class LinkedList:
 
     def to_plain_list(self):
         """Recursively converts linked list to a regular Python list."""
+        plain_list = []
+        current = self._head
+        while current is not None:
+            plain_list.append(current.data)
+            current = current.next
+        return plain_list
 
-        def _to_plain_list(node, acc):
-            if node is None:
-                return acc
-            acc.append(node.data)
-            return _to_plain_list(node.next, acc)
-
-        return _to_plain_list(self._head, [])
 
     def display(self):
         """Displays contents of list."""
@@ -107,24 +106,24 @@ class LinkedList:
         print("None")
 
 
-# # Example usage
-# ll = LinkedList()
-# ll.add(3)
-# ll.add(2)
-# ll.add(1)
-# ll.display()  # Expected: 1 -> 2 -> 3 -> None
-#
-# ll.insert(2, 4)
-# ll.display()  # Expected: 1 -> 2 -> 4 -> 3 -> None
-#
-# ll.remove()
-# ll.display()  # Expected: 1 -> 2 -> 4 -> None
-#
-# ll.reverse()
-# ll.display()  # Expected: 4 -> 2 -> 1 -> None
-#
-# print("Contains 2:", ll.contains(2))  # Expected: True
-# print("Contains 5:", ll.contains(5))  # Expected: False
-#
-# plain_list = ll.to_plain_list()
-# print("Plain list:", plain_list)  # Expected: [4, 2, 1]
+# Example usage
+ll = LinkedList()
+ll.add(3)
+ll.add(2)
+ll.add(1)
+ll.display()  # Expected: 1 -> 2 -> 3 -> None
+
+ll.insert(2, 4)
+ll.display()  # Expected: 1 -> 2 -> 4 -> 3 -> None
+
+ll.remove()
+ll.display()  # Expected: 1 -> 2 -> 4 -> None
+
+ll.reverse()
+ll.display()  # Expected: 4 -> 2 -> 1 -> None
+
+print("Contains 2:", ll.contains(2))  # Expected: True
+print("Contains 5:", ll.contains(5))  # Expected: False
+
+plain_list = ll.to_plain_list()
+print("Plain list:", plain_list)  # Expected: [4, 2, 1]
